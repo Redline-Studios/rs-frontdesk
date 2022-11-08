86d2420a14a2b47d5f78fd13d91ed5c1173106b8
xThrasherrr <101474430+xThrasherrr@users.noreply.github.com>
2022-11-07 21:23:33 -0800

    Multiple Jobs + Locations + Menu Config
    - Adds multiple jobs
    - Multiple Locations
    - Config the menu from config.lua

--- shared/sh_config.lua
+++ shared/sh_config.lua
@@ -1,12 +1,37 @@
 Config = {}
 Config.Debug = false -- True / False for Debug System
 Config.Dispatch = "ps-dispatch" -- Default / ps-dispatch / cd-dispatch
-Config.Jobs = {"police"} -- PD Job Name
 
 Config.Locations = {
-    ["police"] = {
-        [1] = { name = "MRPD-FrontDesk", coords = vector3(442.44, -979.91, 30.69), length = 0.8, width = 0.6, heading = 335, minZ = 30.49, maxZ = 31.29
+    ["police"] = { -- Set this to the name of the job
+        Zone = { -- Polyzone Info
+            name = "MRPD Front Desk", -- Name of the menu title
+            coords = vector3(442.44, -979.91, 30.69), -- Boxzone Coords
+            length = 0.8, width = 0.6, -- Boxzone Length / Width
+            heading = 335, -- Boxzone Heading
+            minZ = 30.49, -- Boxzone MinZ
+            maxZ = 31.29 -- Boxzone MaxZ
         },
+        Menu = { -- Menu Info ( Set Header, Text, Icon, Event, and Event Type )
+            [1] = { Header = 'Assistance', Txt = '', Icon = 'fas fa-hand', Event = 'rs-frontdesk:client:RequestAssistance', Args = 'assistance' },
+            [2] = { Header = 'Weapon License', Txt = '', Icon = 'fas fa-gun', Event = 'rs-frontdesk:client:RequestAssistance', Args = 'weaponlicense' },
+            [3] = { Header = 'Interview', Txt = '', Icon = 'fas fa-people-arrows-left-right', Event = 'rs-frontdesk:client:RequestAssistance', Args = 'interview' },
+            [4] = { Header = 'Supervisor', Txt = '', Icon = 'fas fa-crown', Event = 'rs-frontdesk:client:RequestAssistance', Args = 'supervisor' },
+        }
+    },
+    ["ambulance"] = {
+        Zone = {
+            name = "Pillbox Front Desk",
+            coords = vector3(311.81, -593.42, 43.28),
+            length = 0.8, width = 0.8,
+            heading = 337,
+            minZ = 42,
+            maxZ = 44
+        },
+        Menu = {
+            [1] = { Header = 'Assistance', Txt = '', Icon = 'fas fa-hand', Event = 'rs-frontdesk:client:RequestAssistance', Args = 'assistance'},
+            [2] = { Header = 'Interview', Txt = '', Icon = 'fas fa-people-arrows-left-right', Event = 'rs-frontdesk:client:RequestAssistance', Args = 'interview' },
+            [3] = { Header = 'Supervisor', Txt = '', Icon = 'fas fa-crown', Event = 'rs-frontdesk:client:RequestAssistance', Args = 'supervisor'},
+        }
     }
 }
-
